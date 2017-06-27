a055606764a41cc6b55d400e26abaea155f5af37
Sylvain <sboulade@gmail.com>
2017-06-27 10:27:49 +0200

    Implement the login mutation

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,8 +1,45 @@
 import React from "react";
 
 class AuthForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.updateInput = this.updateInput.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    const { email, password } = this.state;
+    this.props.login({ email, password });
+  }
+
+  updateInput(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <form>AuthForm</form>;
+    return (
+      <div className="row">
+        <form className="col s4">
+          <div className="input-field">
+            <label>Email:</label>
+            <input name="email" value={this.state.email} onChange={this.updateInput} />
+          </div>
+          <div className="input-field">
+            <label>Password:</label>
+            <input name="password" value={this.state.password} onChange={this.updateInput} />
+          </div>
+          <button className="btn" onClick={this.submitForm}>Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { graphql } from "react-apollo";
 import AuthForm from "./AuthForm";
+import login from "../mutations/login";
 
 class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.login = this.login.bind(this);
+  }
+
+  login({ email, password }) {
+    this.props.mutate({
+      variables: {
+        email,
+        password
+      }
+    });
+  }
+
   render() {
-    return <AuthForm />;
+    return (
+      <div className="container">
+        <h3>Login</h3>
+        <AuthForm login={this.login} />
+      </div>
+    );
   }
 }
 
-export default LoginForm;
+export default graphql(login)(LoginForm);
